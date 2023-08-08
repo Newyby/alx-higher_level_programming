fd003908e89c3e87ea3cb7320be869488c622cad
Akinsanmi omobonike <officialraydata@gmail.com>
2023-08-08 09:33:22 -0700

    Redo task

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+# 101-remove_char_at.py
 def remove_char_at(str, n):
-  if 0 <= n < len(str):
-     return (str.replace(str[n], ""))
-  else:
-     return (str)
+    """Create a copy of the string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
