e3ddeae8ece2b77fdc8ef4d2be7f97789c751c5d
Akinsanmi omobonike <officialraydata@gmail.com>
2023-11-30 03:42:50 -0800

    All t

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,2 +1,3 @@
-y methods
-curl -s -X POST $1 -d @"$2" --header "Content-Type: application/json"
+#!/bin/bash
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
